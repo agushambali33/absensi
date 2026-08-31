85f5d76d5e15f0611998977f3fad0bebccade098
Agushambali33 <akunpremiumaing@gmail.com>
2026-08-31 23:50:39 +0700

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,4 @@
+// Ganti dengan URL Web App Google Apps Script setelah backend dideploy.
+// Jika kosong, aplikasi otomatis menggunakan demo/local mode.
+export const API_URL = "";
+export const APP_NAME = "Ruang Belajar";
