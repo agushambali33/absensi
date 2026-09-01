172b303569647fbd9d30d9b177a1dff832cf15bd
Agushambali33 <akunpremiumaing@gmail.com>
2026-09-01 16:35:49 +0700

    Delete src/config.js

--- src/config.js
+++
@@ -1,4 +0,0 @@
-// Ganti dengan URL Web App Google Apps Script setelah backend dideploy.
-// Jika kosong, aplikasi otomatis menggunakan demo/local mode.
-export const API_URL = "https://script.google.com/macros/s/AKfycbymNoutiq-4fji_03n2CpbSvSQUusLb17rODp12e7mNiCGBtMhNGjPEs9PtrZY1BFSzWA/exec";
-export const APP_NAME = "Ruang Belajar";
